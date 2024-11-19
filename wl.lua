60b150ce4dc3c11195825524bdceb9ec4c4e41b4
HenriquePog <77375377+HenriquePog@users.noreply.github.com>
2024-11-19 20:03:55 +0000

    Adicionando HWID hwid1234

--- wl.lua
+++ wl.lua
@@ -1,3 +1,4 @@
 return {
-    -- "hwid1" -- discord_id, data_que_foi_adicionado
+   -- "hwid1" -- discord_id, data_que_foi_adicionado
+   "hwid1234", -- 123456789012345678, 2024-11-19 20:03:58, 828
 }
