495c23e1767a29259910a0b0ec415b08844bac85
HenriquePog <77375377+HenriquePog@users.noreply.github.com>
2024-11-20 03:40:17 +0000

    Atualização da whitelist

--- wl.lua
+++ wl.lua
@@ -1,5 +1,2 @@
 return {
-   "FOI", -- 882814632488747078, 2024-11-20 03:31:23, 599
-   "HuckoreaPedofiloEAssediador", -- <@882814632488747078>-identificar, 2024-11-20 03:39:49, 921
-   "HuckoreaPedofiloEAssediador", -- <@882814632488747078>-wl, 2024-11-20 03:39:58, 857
 }
